f190ce260bdf2728ea322d41abbad11e1b2c8f61
himanidudeja <himanidudeja97857@gmail.com>
2026-03-27 12:15:34 +0530

    Code added

--- src/main/java/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,67 +2,65 @@ package QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 
-
-
-    // Inner class for Feet
-
     static class Feet {
-
         private final double value;
-
-
-
-        // Constructor
-
         public Feet(double value) {
-
             this.value = value;
-
         }
 
-
-
-        // Override equals method
-
         @Override
-
         public boolean equals(Object obj) {
 
             if (this == obj) return true;
-
-
-
             if (obj == null || getClass() != obj.getClass())
-
                 return false;
-
-
-
             Feet other = (Feet) obj;
-
-
-
             return Double.compare(this.value, other.value) == 0;
-
         }
+    }
+
+    static class Inches {
+        private final double value;
+        public Inches(double value) {
+            this.value = value;
+        }
+        
+        @Override
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+
+    public static void testFeetEquality() {
+        Feet f1 = new Feet(5.0);
+        Feet f2 = new Feet(5.0);
+        System.out.println("Feet equal? " + f1.equals(f2));
 
     }
 
 
+    public static void testInchesEquality() {
 
-    // Main method to test
+        Inches i1 = new Inches(10.0);
+
+        Inches i2 = new Inches(10.0);
+
+        System.out.println("Inches equal (same values)? " + i1.equals(i2));
+
+
+    }
 
     public static void main(String[] args) {
 
-    	//SpringApplication.run(MeasurementApplication.class,args);
+        testFeetEquality();
 
-        Feet f1 = new Feet(5.0);
-
-        Feet f2 = new Feet(5.0);
-
-
-
-        System.out.println("Are equal? " + f1.equals(f2));
+        testInchesEquality();
 
     }
 
--- src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
+++ src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MeasurementApplicationTests {
-
     @Test
     void testFeetEquality_SameValue() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(89.6);
@@ -41,7 +40,68 @@ class MeasurementApplicationTests {
     void testFeetEquality_SameReference() {
         QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(89.6);
    
-
         assertTrue(f1.equals(f1));
     }
+    
+    
+    
+    @Test
+
+    void testInchesEquality_SameValue() {
+
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(10.5);
+
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(10.5);
+
+
+
+        assertEquals(i1, i2);
+
+    }
+
+
+
+    @Test
+
+    void testInchesEquality_DifferentValue() {
+
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(10.5);
+
+        QuantityMeasurementApp.Inches i2 = new QuantityMeasurementApp.Inches(5.2);
+
+
+
+        assertNotEquals(i1, i2);
+
+    }
+
+
+
+    @Test
+
+    void testInchesEquality_NullValue() {
+
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(7.0);
+
+
+
+        assertNotEquals(i1, null);
+
+    }
+
+
+
+    @Test
+
+    void testInchesEquality_DifferentType() {
+
+        QuantityMeasurementApp.Inches i1 = new QuantityMeasurementApp.Inches(7.0);
+
+        String other = "7.0";
+
+        assertNotEquals(i1, other);
+
+    }
+
+
 }
