9499e26f2c5db0274118845198984a8a59018f86
himanidudeja <himanidudeja97857@gmail.com>
2026-04-24 10:03:48 +0530

    UC10 code added

--- src/main/java/com/quantity/measurement/enums/WeightUnit.java
+++ src/main/java/com/quantity/measurement/enums/WeightUnit.java
@@ -1,22 +1,36 @@
 package com.quantity.measurement.enums;
 
-public enum WeightUnit {
-	    KILOGRAM(1.0),
-	    GRAM(0.001),
-	    POUND(0.453592);
+public enum WeightUnit implements IMeasurable {
 
-	    private final double conversionFactor;
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(1.0 / 2.20462);//
+    private final double toKilogramFactor;
 
-	    WeightUnit(double conversionFactor) {
-	        this.conversionFactor = conversionFactor;
-	    }
+    WeightUnit(double toKilogramFactor) {
+        this.toKilogramFactor = toKilogramFactor;
+    }
 
-	    public double convertToBaseUnit(double value) {
-	        return value * this.conversionFactor;
-	    }
+    @Override
+    public double getConversionFactor() {
+        return toKilogramFactor;
+    }
 
-	    public double convertFromBaseUnit(double valueInBase) {
-	        return valueInBase / this.conversionFactor;
-	    }
-	}
+    @Override
+    public double convertToBaseUnit(double value) {
+        validate(value);
+        return value * toKilogramFactor;
+    }
 
+    @Override
+    public double convertFromBaseUnit(double value) {
+        validate(value);
+        return value / toKilogramFactor;
+    }
+
+    private void validate(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
+    }
+}
